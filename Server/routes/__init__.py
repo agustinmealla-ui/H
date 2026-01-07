ac09fd594d9e878a44248d6a071eb9c8230f8bbe
agustinmealla-ui <agustinmealla@gmail.com>
2026-01-06 21:49:30 -0300

    delete all except docs

--- Server/routes/__init__.py
+++
@@ -1,4 +0,0 @@
-from .ticker import router as ticker_router
-from .chat import router as chat_router
-
-__all__ = ["ticker_router", "chat_router"]
--- Server/services/__init__.py
+++
@@ -1,5 +0,0 @@
-from .market import market_service
-from .sec import sec_service
-from . import metrics
-
-__all__ = ["market_service", "sec_service", "metrics"]
